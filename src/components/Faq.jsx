825462d7fab2b29518ba319ebea47171a9f4883f
caaassio <caaassio@gmail.com>
2025-12-30 21:04:49 -0300

    faq-alt

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,6 +3,11 @@ import './Faq.css'
 
 const faqData = [
   {
+    question: 'Qual o valor da sessão?',
+    answer:
+    'De acordo com o Código de Ética Profissional do Psicólogo, a divulgação pública de valores não é permitida. Os honorários são informados de forma individual através do whatsapp. Caso tenha interesse, fico à disposição para esclarecer essas informações de forma privada.'
+  },
+  {
     question: 'Quanto tempo dura uma sessão?',
     answer: '50 minutos.'
   },
@@ -25,6 +30,21 @@ const faqData = [
     question: 'A terapia funciona para todos?',
     answer:
       'A terapia pode ser benéfica para qualquer pessoa, mas os resultados dependem do compromisso do paciente e da relação com o terapeuta.'
+  },
+  {
+    question: 'Como funciona a psicoterapia?',
+    answer:
+    'A psicoterapia é um espaço de escuta, acolhimento e reflexão, onde você pode falar livremente sobre suas emoções, vivências e dificuldades. O processo acontece de forma gradual, respeitando seu tempo, com o objetivo de promover autoconhecimento, elaboração emocional e mudanças mais conscientes.'
+  },
+  {
+    question: 'O atendimento é online ou presencial? Como funciona o atendimento online? ',
+    answer:
+    'Atendo exclusivamente de forma online. As sessões acontecem por videochamada, em um horário previamente combinado. É importante estar em um local privado, com boa conexão à internet, garantindo conforto, sigilo e qualidade no atendimento.'
+  },
+  {
+    question: 'A terapia online é eficaz?',
+    answer:
+    'Sim. A terapia online é reconhecida e regulamentada pelo Conselho Federal de Psicologia e apresenta resultados tão eficazes quanto o atendimento presencial, desde que realizada de forma ética e com um bom vínculo terapêutico.'
   },
 ]
 
